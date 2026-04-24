759dc98a33cdf511ef962cb97fced68ce7897a6f
Hanok Alure <hanokalure@gmail.com>
2026-04-24 07:37:50 +0530

    testing

--- mobile/src/services/supabaseClient.js
+++ mobile/src/services/supabaseClient.js
@@ -1,3 +1,4 @@
+import "react-native-url-polyfill/auto";
 import { createClient } from "@supabase/supabase-js";
 
 /**
@@ -7,4 +8,12 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    // Disable Supabase Auth since we use FastAPI.
+    // This prevents a FATAL crash in React Native because it automatically tries to access `localStorage` by default
+    persistSession: false,
+    autoRefreshToken: false,
+    detectSessionInUrl: false,
+  },
+});
